ee40742c8655926294746f937c9f1a4e3792ed5c
Ester Vieira <vieirarester@gmail.com>
2025-05-06 23:48:58 -0300

    feat: adicionando rota POST e ajustando README

--- routes/musicas.js
+++ routes/musicas.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Simulando um banco de dados com um array
-const musicas = [
+let musicas = [
     { titulo: "Believer", artista: "Imagine Dragons" },
     { titulo: "Judas", artista: "Lady Gaga" },
     { titulo: "Billie Jean", artista: "Michael Jackson" },
@@ -10,9 +10,21 @@ const musicas = [
     { titulo: "Diamonds", artista: "Rihanna" }
 ];
 
-// Rota GET para listar músicas
+// GET /api/musicas
 router.get('/', (req, res) => {
     res.status(200).json(musicas);
+});
+
+// POST /api/musicas
+router.post('/', (req, res) => {
+    const novaMusica = req.body;
+
+    // Simples verificação
+    if (!novaMusica.titulo || !novaMusica.artista) {
+        return res.status(400).json({ erro: 'Título e artista são obrigatórios.' });
+    }
+
+    res.status(201).json(novaMusica);
 });
 
 module.exports = router;
